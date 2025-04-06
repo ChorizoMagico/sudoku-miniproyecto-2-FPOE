0ef578fa9240286d57bfe182ef97f66bb0207ec2
ChorizoMagico <juanestebanarias.2006@gmail.com>
2025-04-06 18:46:37 -0500

    Plantilla del proyecto creada por intellij idea

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.sudokuminiproyecto2fpoe {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.sudokuminiproyecto2fpoe to javafx.fxml;
+    exports com.example.sudokuminiproyecto2fpoe;
+}
